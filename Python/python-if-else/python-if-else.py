c864fd421e3925e4ddf39503d374226d5e0d00cb
Rutendo Charmaine Nyakutira <91821484+burnedfenrir@users.noreply.github.com>
2021-10-14 14:17:17 +0200

    python if else
    - Solution file added
    - Problem statement added
    - Testcases added

---
+++ Python/python-if-else/python-if-else.py
@@ -0,0 +1,18 @@
+import math
+import os
+import random
+import re
+import sys
+
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
+    
+if(n%2==0):
+    if(n<=20 and n>=6):
+        print("Weird");
+    else:
+        print("Not Weird");
+else:
+    print("Weird");
